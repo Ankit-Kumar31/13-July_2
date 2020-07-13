f30ef5ed8ebe70dc4cb5c4e02f3d5dbd01a21f0e
Your Name <you@example.com>
2020-07-13 15:34:58 +0530

    initial commit

---
+++ Avg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,c,sum, avg;
+            Console.WriteLine("Enter first, second and third number");
+            a = Convert.ToInt16(Console.ReadLine());
+            b = Convert.ToInt16(Console.ReadLine());
+            c = Convert.ToInt16(Console.ReadLine());
+            sum = a + b + c;
+            avg = sum / 3;
+            Console.WriteLine(sum);
+            Console.WriteLine(avg);
+         }
+
+    }
+}
